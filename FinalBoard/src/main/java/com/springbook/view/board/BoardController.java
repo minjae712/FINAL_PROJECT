54ab40b9b7903d1bd6c79e388aff7b3aee40bd2c
kyeong-sik <boreds@daum.net>
2021-06-07 18:19:31 +0900

    commit

--- FinalBoard/src/main/java/com/springbook/view/board/BoardController.java
+++ FinalBoard/src/main/java/com/springbook/view/board/BoardController.java
@@ -118,7 +118,7 @@ public class BoardController {
 		vo.setSearchKeyword(vo.getSearchKeyword());
 		model.addAttribute("noticeList",boardService.getNoticeList());
 		model.addAttribute("pages",result);
-		return "getBoardList.jsp";
+		return "blog.jsp";
 	}
 	
 	@RequestMapping(value = "/insertNotice.do")
